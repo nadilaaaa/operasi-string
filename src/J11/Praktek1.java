73505ae47dc035f185672c55501ffae2b43b84c5
WINDOWS 10 <WINDOWS 10@DESKTOP-AVSMGMS>
2018-08-14 13:00:35 +0700

    COBA METHOD LENGTH & isEmpty

--- src/J11/Praktek1.java
+++ src/J11/Praktek1.java
@@ -13,5 +13,18 @@ public class Praktek1 {
     public static void main(String[] args) {
         String nama = "Nadila Amalia Pribadi / XRPL3 / 29";
         System.out.println("Identitas: " + nama);
+        
+        String x = "Operasi";
+        System.out.println("isi variabel x: " + x);
+        System.out.println("\"" + x + "\" panjang karakter = " + x.length());
+        
+        System.out.println("x adalah kosong : " + x.isEmpty());
+        
+        String y = "";
+        System.out.println("isi variabel y: " + y);
+        System.out.println("y adalah kosong: " + y.isEmpty());
+        
+        System.out.println("===================================");
+        
 }
 }

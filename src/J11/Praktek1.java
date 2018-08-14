3bd587ffa978508e590d53732e22107f59545cca
WINDOWS 10 <WINDOWS 10@DESKTOP-AVSMGMS>
2018-08-14 12:59:03 +0700

    Initial Commit

---
+++ src/J11/Praktek1.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package J11;
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public class Praktek1 {
+    public static void main(String[] args) {
+        String nama = "Nadila Amalia Pribadi / XRPL3 / 29";
+        System.out.println("Identitas: " + nama);
+}
+}

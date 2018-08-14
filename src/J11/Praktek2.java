fde5951082d1e39724d1841a73d8737698618086
WINDOWS 10 <WINDOWS 10@DESKTOP-AVSMGMS>
2018-08-14 17:59:39 +0700

    Initial Commit

---
+++ src/J11/Praktek2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package J11;
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public class Praktek2 {
+    private static void tampilJudul(String identitas) {
+       System.out.println("Identitas: " + identitas);
+       
+       System.out.println("\nConvert Kalimat Alay Angka (Vokal Ke Angka) \n");
+   }
+         //BBBB
+    public static void main(String[] args) {
+       String identitas = "Nadila Amalia Pribadi / XRPL3 / 29";
+       
+       tampilJudul(identitas); 
+    }
+}
